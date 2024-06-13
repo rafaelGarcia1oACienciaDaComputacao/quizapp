055859dabab835e975212ddb2ef79e9bb16f23f6
Rafael Garcia <rafael_garcia@educadventista.org>
2024-06-13 20:41:11 +0000

    Criação de um helper responsável por mudar o tema

--- script.js
+++ script.js
@@ -1,31 +1,13 @@
-let tema = "claro"
+import { trocarTema, verificarTema } from "./helpers/tema-helper.js"
+
 
 const botaoTema = document.querySelector(".tema button")
 const body = document.querySelector("body")
-botaoTema.addEventListener("click", trocarTema)
 
-function trocarTema() {
-    if (localStorage.getItem("tema")) {
-        tema = localStorage.getItem("tema")
-    }
+botaoTema.addEventListener("click", () => {
+    trocarTema(body, botaoTema)
+})
 
-    if (tema === "claro") {
-        body.classList.add("escuro")
-        localStorage.setItem("tema", "escuro")
-    } else {
-        body.classList.remove("escuro")
-        localStorage.setItem("tema", "claro")
-    }
-}
 
-function verificarTema() {
-    if (localStorage.getItem("tema")) {
-        tema = localStorage.getItem("tema")
-    }
 
-    if (tema === "escuro") {
-        body.classList.add("escuro")
-    }
-}
-
-verificarTema()
+verificarTema(body, botaoTema)
