5d9ac7db81eedc5223a33648fafeaceaf3e2fd0f
Rafael Garcia <rafael_garcia@educadventista.org>
2024-06-13 23:07:02 +0000

    Alteração dinâmica do assunto da página

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -8,6 +8,19 @@ botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema)
 })
 
-
-
 verificarTema(body, botaoTema)
+
+const assunto = localStorage.getItem("assunto")
+
+function alterarAssunto() {
+    const divIcone = document.querySelector(".assunto_icone")
+    const iconeImg = document.querySelector(".assunto_icone img")
+    const assuntoTitulo = document.querySelector(".assunto h1")
+
+    divIcone.classList.add(assunto.toLowerCase())
+    iconeImg.setAttribute("src", `../../assets/images/icon-${assunto.toLowerCase()}.svg`)
+    iconeImg.setAttribute("alt", `Ícone de ${assunto}`)
+    assuntoTitulo.innerText = assunto
+}
+
+alterarAssunto()
